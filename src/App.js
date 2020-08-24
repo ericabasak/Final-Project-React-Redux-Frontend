cf4014488da8052607af7718f0a3a217fe70ab14
ericabasak <ericabasak@gmail.com>
2020-08-24 14:48:12 -0400

    added the components folder with todos and todoitems

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>TodoApp</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        is_complete: false
+      },
+      {
+        id: 2,
+        title: "go to grocery store",
+        is_complete: true
+      },
+      {
+        id: 3,
+        title: "moew lawn",
+        is_complete: false
+      }
+    ]
+
+  }
+
+  render() {
+    // console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
