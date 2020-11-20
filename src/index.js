1bfe25a2d16b922d4ccdc2b426714d52291a5251
ericabasak <ericabasak@gmail.com>
2020-11-20 10:38:14 -0500

    made changes to add redux to the index js and added a reducers folder and created the todoappreducer file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import toDoAppReducer from './reducers/toDoAppReducer';
 // import {
 //   createStore,
 //   applyMiddleware
 // } from 'redux';
 // import thunk from 'redux-thunk';
-// import { Provider } from 'react-redux';
 
-// const store = createStore()
+const store = createStore(toDoAppReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
