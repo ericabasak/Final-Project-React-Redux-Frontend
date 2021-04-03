3c749742cbd4f441fe3203e1f6a42ebb8ed3f012
ericabasak <ericabasak@gmail.com>
2021-04-03 12:39:24 -0400

    more object destructuring

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,21 +7,19 @@ import UserLoginForm from './UserLoginForm';
 class HomePage extends Component {
 
   render() {
+    const { username } = this.props.user
+
     if (this.props.token) {
       return (
         <div style={{ textAlign: "center", padding: "90px" }}>
           <h2>Organize your life with TodoApp</h2>
-          {this.props.user.username &&
-            <h4>Welcome, {this.props.user.username}</h4>}
-          {!this.props.user.username &&
-            <h5>You are not currently logged in</h5>}
+          {username && <h4>Welcome, {username}</h4>}
+          {!username && <h5>You are not currently logged in</h5>}
           <Logout />
         </div>
       )
     } else {
-      return (
-        <UserLoginForm />
-      );
+      return ( <UserLoginForm /> );
     }
   }
 }
